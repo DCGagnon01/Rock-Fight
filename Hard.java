57446d67cc0f83c865d1caf94844742e469423e3
DCGagnon01 <deancgagnon7@gmail.com>
2015-11-12 10:38:17 -0500

    Menu

---
+++ Hard.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Hard here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hard extends BackDrop
+{
+
+    /**
+     * Constructor for objects of class Hard.
+     * 
+     */
+    public Hard()
+    {
+        removeMainMenu();
+    }
+}
---
+++ Normal.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Normal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Normal extends BackDrop
+{
+
+    /**
+     * Constructor for objects of class Normal.
+     * 
+     */
+    public Normal()
+    {
+        removeMainMenu();
+    }
+}
