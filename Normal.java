f6d40076aa45c3a5f64709cd90dc55fe00650992
joman394 <jordan.owens1@hotmail.com>
2015-11-12 11:07:28 -0500

    Added Normal

---
+++ Normal.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Normal extends BackDrop
+{
+    public void act()
+    {
+
+        if(normal = 1000)
+        {
+            
+        }
+        else
+        {
+            normal++;
+        }
+    }
+
+}
+
